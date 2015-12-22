0dad8266a124df210382a16462481fa639463fe4
sir-oga <sir-oga@mail.ru>
2015-12-22 18:25:55 +0200

    start project

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://peterpan.salsadance.dp.ua/admin/');
+define('HTTP_CATALOG', 'http://peterpan.salsadance.dp.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://peterpan.salsadance.dp.ua/admin/');
+define('HTTPS_CATALOG', 'http://peterpan.salsadance.dp.ua/');
+
+// DIR
+define('DIR_APPLICATION', '/home/silezkiy/salsadance.dp.ua/peterpan/admin/');
+define('DIR_SYSTEM', '/home/silezkiy/salsadance.dp.ua/peterpan/system/');
+define('DIR_LANGUAGE', '/home/silezkiy/salsadance.dp.ua/peterpan/admin/language/');
+define('DIR_TEMPLATE', '/home/silezkiy/salsadance.dp.ua/peterpan/admin/view/template/');
+define('DIR_CONFIG', '/home/silezkiy/salsadance.dp.ua/peterpan/system/config/');
+define('DIR_IMAGE', '/home/silezkiy/salsadance.dp.ua/peterpan/image/');
+define('DIR_CACHE', '/home/silezkiy/salsadance.dp.ua/peterpan/system/cache/');
+define('DIR_DOWNLOAD', '/home/silezkiy/salsadance.dp.ua/peterpan/system/download/');
+define('DIR_UPLOAD', '/home/silezkiy/salsadance.dp.ua/peterpan/system/upload/');
+define('DIR_LOGS', '/home/silezkiy/salsadance.dp.ua/peterpan/system/logs/');
+define('DIR_MODIFICATION', '/home/silezkiy/salsadance.dp.ua/peterpan/system/modification/');
+define('DIR_CATALOG', '/home/silezkiy/salsadance.dp.ua/peterpan/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'silezkiy.mysql.ukraine.com.ua');
+define('DB_USERNAME', 'silezkiy_pp');
+define('DB_PASSWORD', 'w7cftnn5');
+define('DB_DATABASE', 'silezkiy_pp');
+define('DB_PREFIX', 'oc_');
